4222eadc596fe53dc2cdcc79d84c9bf30c901a84
Tiago <tiagaoalb@gmail.com>
2023-12-01 12:29:12 -0300

    chore: add initial implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TutorService } from './tutor.service'
+import { Tutor } from './tutor'
+import { HttpErrorResponse } from '@angular/common/http'
+import { PetService } from './pet.service'
+import { Pet } from './pet'
 
 @Component({
   selector: 'app-root',
@@ -9,6 +14,42 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tutor-pet-app';
+
+  public tutors: Tutor[] = [];
+  public pets: Pet[] = [];
+
+  constructor(
+    private tutorService: TutorService,
+    private petService: PetService
+  ) { }
+  
+  ngOnInit(): void {
+    this.getTutors();
+  }
+
+  public getTutors(): void {
+    this.tutorService.getTutors().subscribe(
+      (response: Tutor[]) => {
+        this.tutors = response;
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public getPets(): void {
+    this.petService.getPets().subscribe(
+      (response: Pet[]) => {
+        this.pets = response;
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
