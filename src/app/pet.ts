e461af4d2eac25976958ac3475f215a34c1a292d
Tiago <tiagaoalb@gmail.com>
2023-12-01 12:29:37 -0300

    chore: add pet interface

---
+++ src/app/pet.ts
@@ -0,0 +1,10 @@
+export interface Pet {
+    id: number;
+    name: string;
+    petBreed: string;
+    birthDate: string;
+    color: string;
+    weight: number;
+    vaccineDate: string;
+    vaccineType: string;
+}
